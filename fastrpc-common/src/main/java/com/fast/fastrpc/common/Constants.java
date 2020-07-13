b4296c1cb1ce4a7614c77377d18c79653855b725
zonghaishang <yiji@apache.org>
2020-07-13 23:44:51 +0800

    update license.

--- fastrpc-common/src/main/java/com/fast/fastrpc/common/Constants.java
+++ fastrpc-common/src/main/java/com/fast/fastrpc/common/Constants.java
@@ -1,7 +1,3 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2020 All Rights Reserved.
- */
 package com.fast.fastrpc.common;
 
 /**
--- fastrpc-common/src/main/java/com/fast/fastrpc/common/utils/ConfUtils.java
+++ fastrpc-common/src/main/java/com/fast/fastrpc/common/utils/ConfUtils.java
@@ -1,7 +1,3 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2020 All Rights Reserved.
- */
 package com.fast.fastrpc.common.utils;
 
 import com.fast.fastrpc.common.Constants;
